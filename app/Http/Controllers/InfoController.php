47af41e78e8287c0818d485406c81d2f189aaf3b
kosodov <ploskoviv@mail.ru>
2024-03-13 07:34:39 +0500

    Добавил контроллер InfoController и Роуты к нему

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class InfoController extends Controller
+{
+    public function serverInfo()
+    {
+        return response()->json([
+            'php_version' => phpversion()
+        ]);
+    }
+
+    public function clientInfo(Request $request)
+    {
+        return response()->json([
+            'ip' => $request->ip(),
+            'user_agent' => $request->header('User-Agent')
+        ]);
+    }
+
+    public function databaseInfo()
+    {
+        $databaseVersion = DB::select('SELECT version() as version');
+
+        return response()->json(['database_version' => $databaseVersion]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\InfoController;
+
+Route::get('/info/server', [InfoController::class, 'serverInfo']);
+Route::get('/info/client', [InfoController::class, 'clientInfo']);
+Route::get('/info/database', [InfoController::class, 'databaseInfo']);
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
